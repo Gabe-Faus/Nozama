9819e4a354f1c4e8423a3d4e5daf1d5407b224aa
marinapmoreno <marina.pimentelmoreno@gmail.com>
2025-11-12 20:34:28 -0300

    estrutura do seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,51 @@
-import {prisma} from "./";
-
-const main = async () => {
-    await prisma.pato.create({data: })
-
-};
-
-
-//Pato Pato Pato Pato Pato 
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      posts: {
+        create: {
+          title: 'Check out Prisma with Next.js',
+          content: 'https://www.prisma.io/nextjs',
+          published: true,
+        },
+      },
+    },
+  })
+  const bob = await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      posts: {
+        create: [
+          {
+            title: 'Follow Prisma on Twitter',
+            content: 'https://twitter.com/prisma',
+            published: true,
+          },
+          {
+            title: 'Follow Nexus on Twitter',
+            content: 'https://twitter.com/nexusgql',
+            published: true,
+          },
+        ],
+      },
+    },
+  })
+  console.log({ alice, bob })
+}
 main()
-    .them(async() => await prisma.$disconnect())
-    .catch(async (e) => {
-        console.log(e);
-        await prisma.$disconnect();
-    });
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
